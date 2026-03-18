3f86fb75c9739e27575b977be398789dd5b2cffc
Vitor Torres <vitortorresdevv@gmail.com>
2026-03-18 10:13:49 -0300

    primeiro commit

---
+++ app/(tabs)/src/pages/inicial/index.tsx
@@ -0,0 +1,44 @@
+  import { useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+  const router = useRouter();
+
+  export default function Index(){
+    return(
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("../../../../../assets/images/logo.png")}/>
+
+          <TouchableOpacity style={styles.button} onPress={() => router.push("/(tabs)/src/pages/login/")}>
+            <Text style={styles.buttonText}>ENTRAR</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "lightblue",
+      alignItems: "center",
+      justifyContent: "center",
+      flex: 1
+    },
+
+    image: {
+      transform: [{translateY: -40}]
+    },
+
+    button:{
+      backgroundColor: "#132E48",
+      width: "40%",
+      borderRadius: 8,
+      height: 40,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    
+    buttonText:{
+      color: "#ffffff",
+      fontSize: 23,
+      fontWeight: "bold"
+    }
+  });
