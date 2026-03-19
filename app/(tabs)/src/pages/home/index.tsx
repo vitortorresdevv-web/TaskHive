4ca7062ba99d5c9b97bb4e68c810025b8b937407
Vitor Torres <vitortorresdevv@gmail.com>
2026-03-19 15:39:25 -0300

    construção modal

--- app/(tabs)/src/pages/home/index.tsx
+++ app/(tabs)/src/pages/home/index.tsx
@@ -1,6 +1,14 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ModalForm } from '@/components/modal';
+import { useState } from 'react';
+import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Home(){
+    const [modalVisible, setModalVisible] = useState(false);
+
+    function AbrirModal(){
+        setModalVisible(true)
+    }
+
     return(
         <View style={styles.container}>
             <Image style={styles.image} source={require("./imagens/imagemHomeScreen-removebg.png")} />
@@ -10,14 +18,17 @@ export default function Home(){
             </View>
 
             <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.buttonCriar}>
+                <TouchableOpacity style={[styles.button, styles.buttonCriar]} onPress={AbrirModal}>
                     <Text style={styles.textButton}>CRIAR PROJETO</Text>
                 </TouchableOpacity>
                 
-                <TouchableOpacity style={styles.buttonIngressar}>
+                <TouchableOpacity style={[styles.button, styles.buttonIngressar]}>
                     <Text style={styles.textButton}>INGRESSAR</Text>
                 </TouchableOpacity>
             </View>
+            <Modal visible={modalVisible} animationType= "fade" transparent={true}>
+                <ModalForm/>
+            </Modal>
         </View>
     )
 }
@@ -30,11 +41,13 @@ const styles = StyleSheet.create({
     },
 
     image: {
-        transform: [{translateY: -150}]
+        //transform: [{translateY: -150}]
+        marginBottom: 20
     },
 
     text: {
-        transform: [{translateY: -120}],
+        //transform: [{translateY: -120}],
+        marginBottom: 100,
         fontWeight: "bold",
         fontSize: 30,
         color: "#ffffff"
@@ -42,30 +55,32 @@ const styles = StyleSheet.create({
 
     buttonContainer: {
         flexDirection: "row",
-        
+        marginBottom: 180,
+        width: "100%",
+        justifyContent: "space-between",
+    },
+
+    button: {
+        flex: 1,
+        alignItems: "center",
     },
 
     buttonCriar: {
         backgroundColor: "#091d34",
-        borderRadius: 8,
-        alignItems: "center",
-        justifyContent: "center",
-        height: 50,
+        borderRadius: 20,
+        marginRight: 20
     },
 
     buttonIngressar: {
         backgroundColor: "#f99d30",
-        borderRadius: 8,
-        alignItems: "center",
-        justifyContent: "center",
-        height: 50,
-
+        borderRadius: 20,
+        marginLeft: 20
     },
 
     textButton: {
-        color: "#ffffff",
+        color: "white",
         fontWeight: "bold",
-        padding: 8,
-        fontSize: 24
+        padding: 15,
+        fontSize: 19.1,
     }
 })
